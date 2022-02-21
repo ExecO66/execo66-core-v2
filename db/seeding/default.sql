0b3b68ef4456394833f6fc4d6b3cc5103861215e
Kevin <ge.kevin.a@gmail.com>
2022-02-21 02:13:47 -0600

    Add test workflow and fix weird SQL statements (#2)

--- db/seeding/default.sql
+++ db/seeding/default.sql
@@ -1,19 +1,29 @@
+GRANT USAGE ON SCHEMA public TO postgres;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO postgres;
+
+SET search_path TO public;
+
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'public'
+ORDER BY table_name;
+
 BEGIN;
 
-INSERT INTO public.user (id, username, email, user_status, provider, provider_id)
+INSERT INTO "user" (id, username, email, user_status, provider, provider_id)
 VALUES ('00000000-0000-0000-0000-000000000001','Bob Schoolers', 'bob@gmail.com', 'student', 'google', '198a4d');
 
-INSERT INTO public.user (id, username, email, user_status, provider, provider_id)
+INSERT INTO "user" (id, username, email, user_status, provider, provider_id)
 VALUES ('00000000-0000-0000-0000-000000000002','James Madison', 'jm@gmail,com', 'teacher', 'google', '19tjg3');
 
-INSERT INTO public.assignment (id, owner_id, due_date, title, description, code_locations, test_inputs) 
+INSERT INTO assignment (id, owner_id, due_date, title, description, code_locations, test_inputs) 
 VALUES 
 ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 1', 'Some description 1', '{"https://example.com/1"}', 'str::1input1;1input2;1input3;'),
 ('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 2', 'Some description 2', '{"https://example.com/2"}', 'str::2input1;2input2;3input3;' ),
 ('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 3', 'Some description 3', '{"https://example.com/3"}', 'str::3input1;3input2;3input3;' ),
 ('00000000-0000-0000-0000-000000000004', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 4', 'Some description 4', '{"https://example.com/4"}', 'str::4input1;4input2;4input3;' );
 
-INSERT INTO public.assignment_exec (id, assignment_id, output, test_input)
+INSERT INTO assignment_exec (id, assignment_id, output, test_input)
 VALUES
 ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000001', '1output1', '1input1'),
 ('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000001', '1output2', '1input2'),
@@ -31,15 +41,15 @@ VALUES
 ('00000000-0000-0000-0000-000000000011', '00000000-0000-0000-0000-000000000004', '4output2', '4input2'),
 ('00000000-0000-0000-0000-000000000012', '00000000-0000-0000-0000-000000000004', '4output3', '4input3');
 
-INSERT INTO public.student_assignment (student_id, assignment_id)
+INSERT INTO student_assignment (student_id, assignment_id)
 VALUES
 ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002'),
 ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000004');
 
-INSERT INTO public.submission (id, submitter_id, assignment_id, submit_date, code_locations)
+INSERT INTO submission (id, submitter_id, assignment_id, submit_date, code_locations)
 VALUES ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002', now(), '{"http://example.com/1"}');
 
-INSERT INTO public.submission_exec (submission_id, output, test_input)
+INSERT INTO submission_exec (submission_id, output, test_input)
 VALUES
 ('00000000-0000-0000-0000-000000000001', '2sub_exec_output1', '2input1'),
 ('00000000-0000-0000-0000-000000000001', '2sub_exec_output2', '2input2'),
