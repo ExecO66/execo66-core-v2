9e471de6b7074ad1657b2d854f7ed863802121d5
Kevin <ge.kevin.a@gmail.com>
2022-02-20 19:48:26 -0600

    Add queries and query tests

---
+++ internal/entity/queries/insert_user.go
@@ -0,0 +1,41 @@
+package queries
+
+import (
+	"core/internal/entity"
+	"core/internal/entity/enum"
+)
+
+type InsertUserEntity struct {
+	Username   string
+	Email      string
+	UserStatus enum.UserStatus
+	Provider   enum.LoginProvider
+	ProviderId string
+}
+
+type InsertUserModel struct {
+	Id         string
+	Username   string
+	Email      string
+	UserStatus enum.UserStatus
+	Provider   enum.LoginProvider
+	ProviderId string
+}
+
+func InsertUser(user InsertUserEntity) (InsertUserModel, error) {
+	sql := `
+    INSERT INTO public.user (
+        username, 
+        email, 
+        user_status, 
+        provider, 
+        provider_id
+    ) VALUES ($1, $2, $3, $4, $5) 
+    RETURNING id;`
+
+	id := ""
+	err := entity.DbClient.Db.QueryRow(sql, user.Username, user.Email, user.UserStatus, user.Provider, user.ProviderId).Scan(&id)
+
+	return InsertUserModel{Id: id, Username: user.Username, Email: user.Email, UserStatus: user.UserStatus, Provider: user.Provider, ProviderId: user.ProviderId}, err
+
+}
