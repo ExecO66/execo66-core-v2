0f4ed4d5e1a2e1ef5685f0d22b5632b8f23b3359
Kevin <ge.kevin.a@gmail.com>
2022-02-21 15:17:11 -0600

    [skip ci] Insert more test users for queries

--- db/seeding/default.sql
+++ db/seeding/default.sql
@@ -11,17 +11,17 @@ ORDER BY table_name;
 BEGIN;
 
 INSERT INTO "user" (id, username, email, user_status, provider, provider_id)
-VALUES ('00000000-0000-0000-0000-000000000001','Bob Schoolers', 'bob@gmail.com', 'student', 'google', '198a4d');
-
-INSERT INTO "user" (id, username, email, user_status, provider, provider_id)
-VALUES ('00000000-0000-0000-0000-000000000002','James Madison', 'jm@gmail,com', 'teacher', 'google', '19tjg3');
+VALUES 
+('00000000-0000-0000-0000-000000000001','Bob Schoolers', 'bob@gmail.com', 'student', 'google', '198a4d'),('00000000-0000-0000-0000-000000000002','Jimmy Joe', 'jimmy@gmail,com', 'student', 'google', '214vaa'),
+('00000000-0000-0000-0000-000000000003','James Madison', 'james@gmail,com', 'teacher', 'google', 'g119j1'),
+('00000000-0000-0000-0000-000000000004','George Washington', 'george@gmail,com', 'teacher', 'google', 'oiagq1');
 
 INSERT INTO assignment (id, owner_id, due_date, title, description, code_locations, test_inputs) 
 VALUES 
-('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 1', 'Some description 1', '{"https://example.com/1"}', 'str::1input1;1input2;1input3;'),
-('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 2', 'Some description 2', '{"https://example.com/2"}', 'str::2input1;2input2;3input3;' ),
-('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 3', 'Some description 3', '{"https://example.com/3"}', 'str::3input1;3input2;3input3;' ),
-('00000000-0000-0000-0000-000000000004', '00000000-0000-0000-0000-000000000002', now(), 'CS A Lab 4', 'Some description 4', '{"https://example.com/4"}', 'str::4input1;4input2;4input3;' );
+('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000003', now(), 'CS A Lab 1', 'Some description 1', '{"https://example.com/1"}', 'str::1input1;1input2;1input3;'),
+('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000003', now(), 'CS A Lab 2', 'Some description 2', '{"https://example.com/2"}', 'str::2input1;2input2;3input3;' ),
+('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000003', now(), 'CS A Lab 3', 'Some description 3', '{"https://example.com/3"}', 'str::3input1;3input2;3input3;' ),
+('00000000-0000-0000-0000-000000000004', '00000000-0000-0000-0000-000000000004', now(), 'CS A Lab 4', 'Some description 4', '{"https://example.com/4"}', 'str::4input1;4input2;4input3;' );
 
 INSERT INTO assignment_exec (id, assignment_id, output, test_input)
 VALUES
@@ -45,6 +45,7 @@ INSERT INTO student_assignment (student_id, assignment_id)
 VALUES
 ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002'),
 ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000004');
+('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000003'),
 
 INSERT INTO submission (id, submitter_id, assignment_id, submit_date, code_locations)
 VALUES ('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000002', now(), '{"http://example.com/1"}');
