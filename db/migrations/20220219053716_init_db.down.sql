bf83da7c81662885e930f7fd9d3958ae450a8971
Kevin <ge.kevin.a@gmail.com>
2022-02-21 00:50:11 -0600

    Fix 'user' Postgres keyword errors

--- db/migrations/20220219053716_init_db.down.sql
+++ db/migrations/20220219053716_init_db.down.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS public.assignment_exec;
 DROP TABLE IF EXISTS public.submission_exec;
 DROP TABLE IF EXISTS public.submission;
 DROP TABLE IF EXISTS public.assignment;
-DROP TABLE IF EXISTS public.user;
+DROP TABLE IF EXISTS "user";
 
 DROP TYPE IF EXISTS user_status_enum;
 DROP TYPE IF EXISTS login_provider_enum;
--- db/migrations/20220219053716_init_db.up.sql
+++ db/migrations/20220219053716_init_db.up.sql
@@ -6,7 +6,7 @@ DROP TYPE IF EXISTS login_provider_enum;
 CREATE TYPE user_status_enum as ENUM ('student', 'teacher');
 CREATE TYPE login_provider_enum as ENUM ('google');
 
-CREATE TABLE IF NOT EXISTS public.user(
+CREATE TABLE IF NOT EXISTS "user"(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     username text NOT NULL,
     email text NOT NULL,
@@ -15,9 +15,9 @@ CREATE TABLE IF NOT EXISTS public.user(
     provider_id text NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.assignment(
+CREATE TABLE IF NOT EXISTS assignment(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    owner_id uuid REFERENCES public.user(id) NOT NULL,
+    owner_id uuid REFERENCES "user"(id) NOT NULL,
     due_date timestamptz NOT NULL,
     title text NOT NULL,
     description text NOT NULL,
@@ -25,30 +25,30 @@ CREATE TABLE IF NOT EXISTS public.assignment(
     test_inputs text NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.assignment_exec(
+CREATE TABLE IF NOT EXISTS assignment_exec(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    assignment_id uuid REFERENCES public.assignment(id) NOT NULL,
+    assignment_id uuid REFERENCES assignment(id) NOT NULL,
     output text NOT NULL,
     test_input text NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.student_assignment(
-    student_id uuid REFERENCES public.user(id) NOT NULL,
-    assignment_id uuid REFERENCES public.assignment(id) NOT NULL,
+CREATE TABLE IF NOT EXISTS student_assignment(
+    student_id uuid REFERENCES "user"(id) NOT NULL,
+    assignment_id uuid REFERENCES assignment(id) NOT NULL,
     PRIMARY KEY (student_id, assignment_id)
 );
 
-CREATE TABLE IF NOT EXISTS public.submission(
+CREATE TABLE IF NOT EXISTS submission(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    submitter_id uuid REFERENCES public.user(id) NOT NULL,
-    assignment_id uuid REFERENCES public.assignment(id) NOT NULL,
+    submitter_id uuid REFERENCES "user"(id) NOT NULL,
+    assignment_id uuid REFERENCES assignment(id) NOT NULL,
     submit_date timestamptz NOT NULL,
     code_locations text[] NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS public.submission_exec(
+CREATE TABLE IF NOT EXISTS submission_exec(
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    submission_id uuid REFERENCES public.submission(id) NOT NULL,
+    submission_id uuid REFERENCES submission(id) NOT NULL,
     output text NOT NULL,
     test_input text NOT NULL
 );
