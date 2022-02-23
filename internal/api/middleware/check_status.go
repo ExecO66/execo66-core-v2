b5843bd6e1eed2fa97db6ce02d20d3cf9af0bdeb
Kevin <ge.kevin.a@gmail.com>
2022-02-22 22:40:10 -0600

    Fix deref assertion on status checking middleware

--- internal/api/middleware/check_status.go
+++ internal/api/middleware/check_status.go
@@ -11,7 +11,7 @@ import (
 // assumes user is authorized
 func CheckStatus(status enum.UserStatus) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		user := c.MustGet("user").(session.SessionUser)
+		user := c.MustGet("user").(*session.SessionUser)
 
 		if user.UserStatus != status {
 			c.Status(http.StatusForbidden)
